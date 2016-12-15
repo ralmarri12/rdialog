efcd73b4ff676ec35ac071e41a8affefa2009a76
Rashid F <r.almarri12@gmail.com>
2016-12-15 22:05:30 +0800

    Correct names

--- index.php
+++ index.php
@@ -12,6 +12,19 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
         <script type="text/javascript" src="assists/js/jquery.js"></script>
-        <script type="text/javascript" src="assists/js/main.js"></script>
+        <script type="text/javascript" src="assists/js/rdialog.js"></script>
+
+
+
+        <script>
+
+        testDialog = new RDialog('System Dialog', 'My content',function(){
+            console.log('callback works !!');
+        });
+
+        testDialog.run();
+
+        </script>
+
     </body>
 </html>
